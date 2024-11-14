d9569d36a8e7fdd485cc04ad741617610b953a18
Eric Alfred <alfrederic371@gmail.com>
2024-11-14 23:28:00 +0100

    [new] group popup

---
+++ src/components/dashboard/GroupPopup.tsx
@@ -0,0 +1,57 @@
+import { X } from "lucide-react";
+
+interface GroupPopupProps {
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+const GroupPopup: React.FC<GroupPopupProps> = ({ isOpen, onClose }) => {
+    if (!isOpen) return null;
+
+    return (
+        <div className="fixed inset-0 bg-[#000000CC] flex items-center justify-center z-50">
+            <div className="relative h-[811px] w-[900px] bg-white rounded-[8px] pt-[48px] px-8 shadow-lg scale-95 animate-scaleIn flex justify-center">
+                <div className="w-[772px] h-[560px] flex flex-col gap-[48px]">
+                    <button
+                        onClick={onClose}
+                        className="absolute -top-[4.5rem] -right-16 w-[64px] h-[64px] bg-white rounded-full p-1 shadow-md flex items-center justify-center"
+                    >
+                        <X className="text-gray-700" />
+                    </button>
+                    <div>
+                        <h2 className="text-[32px] font-semibold leading-[35.2px] tracking-[-1%] text-[#24272E]">Group details</h2>
+                    </div>
+
+                    <form className="space-y-4 w-[772px] flex flex-col gap-[16px]">
+                        <div>
+                            <label className="block text-[20px] font-medium text-[#24272E]">Group name <span className="text-red-500">*</span></label>
+                            <input className="w-full border bg-transparent border-[#CDCED3] rounded-[8px] p-[16px] mt-2" placeholder="Enter group name" />
+                        </div>
+                        <div>
+                            <label className="block text-[20px] font-medium text-[#24272E]">Select customers</label>
+                            <select className="w-full border bg-transparent border-[#CDCED3] rounded-[8px] p-[16px] mt-2">
+                                <option>Select customers</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label className="block text-[20px] font-medium text-[#24272E]">Description</label>
+                            <textarea 
+                                className="w-full border bg-transparent border-[#CDCED3] rounded-[8px] p-[16px] mt-2" 
+                                placeholder="Write a short description for the customer group"
+                                rows={4}
+                            />
+                        </div>
+                        <button
+                            type="button"
+                            className="w-full bg-action-default text-white rounded-[24px] mt-4 hover:opacity-80 h-[67px] p-[16px]"
+                        >
+                            Save
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default GroupPopup;
