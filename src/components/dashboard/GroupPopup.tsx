8ad358f33a8a12909fcd5124c4e93915c132f32b
Eric Alfred <alfrederic371@gmail.com>
2024-11-14 23:46:36 +0100

    [feat] completed customer and group overlay

--- src/components/dashboard/GroupPopup.tsx
+++ src/components/dashboard/GroupPopup.tsx
@@ -28,16 +28,19 @@ const GroupPopup: React.FC<GroupPopupProps> = ({ isOpen, onClose }) => {
                             <input className="w-full border bg-transparent border-[#CDCED3] rounded-[8px] p-[16px] mt-2" placeholder="Enter group name" />
                         </div>
                         <div>
-                            <label className="block text-[20px] font-medium text-[#24272E]">Select customers</label>
-                            <select className="w-full border bg-transparent border-[#CDCED3] rounded-[8px] p-[16px] mt-2">
+                        <label className="block text-[20px] font-medium text-[#24272E] mb-1">Group</label>
+                        <div className="w-full border bg-transparent border-[#CDCED3] rounded-[8px] p-[14px] mt-1">
+                            <select className="h-full w-full bg-transparent outline-none border-none">
                                 <option>Select customers</option>
+                                {/* Add bank options here */}
                             </select>
                         </div>
+                    </div>
                         <div>
                             <label className="block text-[20px] font-medium text-[#24272E]">Description</label>
+                            <label className="mb-3 text-[0.87rem] text-[#5C636D]">Write a short description for the customer group</label>
                             <textarea 
                                 className="w-full border bg-transparent border-[#CDCED3] rounded-[8px] h-[197px] p-[16px] mt-2" 
-                                placeholder="Write a short description for the customer group"
                                 rows={4}
                             />
                         </div>
