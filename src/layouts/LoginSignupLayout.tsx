31771233faa0edb1784d9394e6e9fb4e86b152bf
Eric Alfred <alfrederic371@gmail.com>
2024-11-18 13:26:02 +0100

    Fix: responsiveness

--- src/layouts/LoginSignupLayout.tsx
+++ src/layouts/LoginSignupLayout.tsx
@@ -1,51 +1,55 @@
 import { NavLink, Outlet } from "react-router-dom";
 import loginLogo from "../assets/thrivr-logo.png";
-// import EmailVerification from "@/components/signup/EmailVerification";
 
 const LoginSignupLayout = () => {
   return (
-    <div>
-      <div className="flex">
-      {/* <EmailVerification /> */}
-        <div className="w-[55%] top-0 rounded-r-3xl h-[100%] bg-[#870E73DB] fixed"></div>
-        <div className="w-[45%] px-24 h-screen ms-[55%]">
+    <div className="w-full">
+      <div className="flex flex-col md:flex-row">
+        {/* Remove fixed purple container on mobile, use responsive classes */}
+        <div className="hidden md:block md:w-[55%] md:bg-[#870E73DB] md:rounded-r-3xl fixed top-0 h-full"></div>
+        
+        <div className="w-full md:w-[45%] md:ms-[55%] px-6 md:px-24 min-h-screen flex flex-col">
           <div className="flex gap-1 justify-center mt-5">
             <div className="my-auto w-[80px]">
-              <img src={loginLogo} alt="" className="objext-cover -translate-x-[5px]" />
+              <img 
+                src={loginLogo} 
+                alt="Thrivr Logo" 
+                className="object-cover -translate-x-[5px]" 
+              />
             </div>
-
-            <p className="text-[#870E73] text-4xl font-bold my-auto -translate-x-[25px]">Thrivr</p>
+            <p className="text-[#870E73] text-4xl font-bold my-auto -translate-x-[25px]">
+              Thrivr
+            </p>
           </div>
+          
           <div className="flex justify-center">
             <NavLink
               to="/login"
-              className={({ isActive }) => {
-                return (
-                  "p-3 border-t-4 " +
-                  (isActive ? " border-[#870E73]" : "border-[#3333336E]")
-                );
-              }}
+              className={({ isActive }) => 
+                `p-3 border-t-4 ${
+                  isActive ? "border-[#870E73]" : "border-[#3333336E]"
+                }`
+              }
             >
               Login
             </NavLink>
             <NavLink
               to="/signup"
-              className={({ isActive }) => {
-                return (
-                  "p-3 border-t-4 " +
-                  (isActive ? " border-[#870E73]" : "border-[#3333336E]")
-                );
-              }}
+              className={({ isActive }) => 
+                `p-3 border-t-4 ${
+                  isActive ? "border-[#870E73]" : "border-[#3333336E]"
+                }`
+              }
             >
               Signup
             </NavLink>
           </div>
-          <div className="py-5">
+          
+          <div className="py-5 flex-grow">
             <Outlet />
           </div>
         </div>
       </div>
-      
     </div>
   );
 };
