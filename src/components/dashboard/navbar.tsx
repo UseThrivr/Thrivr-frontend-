6b4da8db183ec640918a78cd716d19f72e874676
Eric Alfred <alfrederic371@gmail.com>
2024-11-25 01:02:18 +0100

    chore: compatibility with sidebar

--- src/components/dashboard/navbar.tsx
+++ src/components/dashboard/navbar.tsx
@@ -3,7 +3,7 @@ import clsx from "clsx";
 const Navbar: React.FC<NavBarProps> = ({ right, left }) => {
     return (
         <nav className={clsx(
-            "flex items-center py-[24px] px-[16px] absolute right-0 top-0 left-[var(--dashboard-sidebar-width)] gap-[511px] bg-white lg:border-b border-solid border-neutral-border",
+            "flex items-center py-[24px] px-[16px] absolute right-0 -top-3 left-[var(--dashboard-sidebar-width)] gap-[511px] bg-transparent lg:border-b border-solid border-neutral-border",
             (left && right) && "justify-between",
             (right && !left) && "justify-end"
         )}>
