cd3839b3a906f1b10ee2f7a416b35986b54f560c
Victor Jack <67293106+thedon-dev@users.noreply.github.com>
2024-12-04 10:20:28 +0100

    update AdComp

--- src/components/ads/AdComp.tsx
+++ src/components/ads/AdComp.tsx
@@ -19,25 +19,25 @@ const AdComp: React.FC<adDetails> = ({ data }) => {
   return (
     <div className="flex justify-between items-center py-5">
       <div className="flex flex-col gap-1">
-        <p className="text-xs font-semibold uppercase text-[#5C636D] mb-2">
+        <p className="text-xs lg:text-base font-semibold uppercase text-[#5C636D] mb-2">
           {data.name}
         </p>
-        <p className="text-xs text-[#870E73]">{data.title}</p>
+        <p className="text-xs lg:text-base text-[#870E73]">{data.title}</p>
         <p className="text-[#5C636D] text-sm">
           {formattedStartDate} - {formattedEndDate}
         </p>
       </div>
       <div className="flex justify-between content-center text-start w-[40%]">
-        <p className="text-sm">To: {data.audience}</p>
+        <p className="text-sm lg:text-base">To: {data.audience}</p>
         <div
           className={`${data.ongoing ? "text-green-500" : "text-red-600"} flex`}
         >
           {data.ongoing ? (
-            <p className="flex text-sm">
+            <p className="flex text-sm lg:text-base">
               <Dot /> Live
             </p>
           ) : (
-            <p className="flex text-sm">
+            <p className="flex text-sm lg:text-base">
               <Check className="p-1" /> Done
             </p>
           )}
