e6fc76c5cb438d4fac5510ba085bb4c241e493b6
Wisdom Ofogba <wisetega007@gmail.com>
2024-12-13 23:56:47 +0100

    updated responsiveness

--- src/pages/mybank.tsx
+++ src/pages/mybank.tsx
@@ -5,6 +5,7 @@ import { ChevronDown, Landmark } from "lucide-react";
 import PaymentBox from "@/components/dashboard/PaymentBox";
 import { useState } from "react";
 import PaymentPopup from "@/components/dashboard/PaymentPopup";
+import WalletCard from "@/components/dashboard/WalletCard";
 
 const Mybank = () => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
@@ -15,29 +16,19 @@ const Mybank = () => {
   return (
     <>
       <Seo title="Bank Accounts" />
-      <section className="w-full h-screen pt-[1rem] px-[5%]">
-        <div className="flex flex-col gap-[4px] mb-5">
+      <section className="w-full h-screen pt-[1rem] md:px-[5%]">
+        <div className="flex flex-col gap-[4px] mb-5 w-full">
           <h1 className="font-[600] text-[48px] leading-[52.8px] tracking-[-1%] text-primary">
             Bank accounts
           </h1>
-          <p className="text-[1rem] w-[512px] leading-[22.4px] font-[500] text-[#5c636d]">
+          <p className="text-[1rem] leading-[22.4px] font-[500] text-[#5c636d]">
             Add both Paystack and Bank Transfers to give buyers option to choose
             and prevent payment delays
           </p>
         </div>
-        <div className="lg:w-[649px] rounded-[8px] flex justify-between p-5 items-center bg-[#FDFBFF] mb-[32px]">
-          <div className="lg:w-[383px] flex flex-col gap-[16px]">
-            <h2 className="font-[500] text-[1rem] text-[5c636d]">
-              Select your preferred currency to receive payment
-            </h2>
-            <div className="w-[244px] h-[30px] flex items-center gap-[16px]">
-              <img src={Nigeria} alt="" className="w-[32px] h-[15.93px]" />
-              <p className="text-[20px] leading-[30px] font-[500] text-[#24272E]">
-                Nigerian Naira (NGN)
-              </p>
-            </div>
-          </div>
-          <ChevronDown className="w-[24px] h-[24px] text-[#5c636d]" />
+        <div className="max-w-[649px] w-full flex gap-4 items-center mb-[32px] overflow-auto invincible-scrollbar">
+          <WalletCard image={Nigeria} chevron={ChevronDown} title="₦43.86" content="Nigerian Naira (NGN)" />
+          <WalletCard title="Open" content="10 currencies" />
         </div>
         <div className="flex flex-col gap-10">
           <PaymentBox
